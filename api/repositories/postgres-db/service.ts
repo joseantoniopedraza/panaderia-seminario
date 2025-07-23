820773184334385866f113288811def8b25ca915
Jose Antonio Pedraza <joseantoniopedraza@MacBook-Pro-de-Jose.local>
2025-07-22 22:05:05 -0400

    test connection

--- api/repositories/postgres-db/service.ts
+++ api/repositories/postgres-db/service.ts
@@ -9,8 +9,6 @@ const createClient = () => {
     host: process.env.PGSQL_DB_HOST || "",
   };
 
-  console.log(config);
-
   return new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: "postgres",
